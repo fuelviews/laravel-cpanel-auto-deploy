bdc02ee8d6a673d3442d9c31c95c04449beb7c40
thejmitchener <joshua@fuelviews.com>
2024-04-04 15:15:57 -0400

    Add cpanel-auto-deploy.sh.stub script for deployment and update CpanelAutoDeployServiceProvider to publish the script to the root directory.

--- src/CpanelAutoDeploy.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Fuelviews\CpanelAutoDeploy;
-
-class CpanelAutoDeploy
-{
-}
--- src/CpanelAutoDeployServiceProvider.php
+++ src/CpanelAutoDeployServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Fuelviews\CpanelAutoDeploy;
 
-use Fuelviews\CpanelAutoDeploy\Commands\CpanelAutoDeployCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,24 @@ class CpanelAutoDeployServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('laravel-cpanel-auto-deploy')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-cpanel-auto-deploy_table')
-            ->hasCommand(CpanelAutoDeployCommand::class);
+            ->name('laravel-cpanel-auto-deploy');
+    }
+
+    public function packageRegistered(): void
+    {
+        $this->app->booted(function () {
+            $this->publishScriptToRoot();
+        });
+    }
+
+    protected function publishScriptToRoot(): void
+    {
+        $sourcePath = __DIR__ . '/../resources/scripts/cpanel-auto-deploy.sh.stub';
+        $destinationPath = base_path('cpanel-auto-deploy.sh');
+
+        copy($sourcePath, $destinationPath);
+
+        chmod($destinationPath, 0755);
     }
 }
