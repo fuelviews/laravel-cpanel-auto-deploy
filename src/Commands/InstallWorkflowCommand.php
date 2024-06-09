fc05eb5e1cc34de090a60eec1a1da0bd85c816f9
thejmitchener <joshua@fuelviews.com>
2024-06-09 18:54:21 -0400

    Add InstallWorkflowCommand.php to the project, which allows for the installation of Fuelviews packages and running their install commands. The command copies a workflow file from a stub to the desired destination, creating any necessary directories along the way.

---
+++ src/Commands/InstallWorkflowCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fuelviews\CpanelAutoDeploy\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+use function Laravel\Prompts\confirm;
+
+class InstallWorkflowCommand extends Command
+{
+    protected $signature = 'deploy:install';
+    protected $description = 'Install all Fuelviews packages and run their install commands';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $source = resource_path('workflows/cpanel-auto-deploy.yml.stub');
+        $destination = base_path('.github/workflows/cpanel-auto-deploy.yml');
+
+        $directory = dirname($destination);
+        if (!File::exists($directory)) {
+            File::makeDirectory($directory);
+        }
+
+        if (File::copy($source, $destination)) {
+            $this->info('Workflow file copied successfully.');
+        } else {
+            $this->error('Failed to copy workflow file.');
+        }
+    }
+}
